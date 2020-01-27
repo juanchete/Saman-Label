19575b637f69396113e71d49c8ae1ca4e1d515e5
Juan Andres Lopez <juanlopezlmg@gmail.com>
2020-01-27 12:00:55 -0400

    entrega1

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from .models import Cliente, Employee ,Department
 
-from .models import Cliente
+class ClienteAdmin(admin.ModelAdmin):
+    list_display=('name', 'identification')
 
-admin.site.register(Cliente)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('id','employee_id','name','last_name','department_name','hours','salary','active')
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display=('id','departmen_name')
+
+# class EmployeeDetailAdmin (admin.ModelAdmin):
+#     list_display=('employee_id','name','department_name','hours','salary','active')
+
+    
+admin.site.register(Cliente,ClienteAdmin)
+admin.site.register(Employee,EmployeeAdmin)
+admin.site.register(Department,DepartmentAdmin)
+# admin.site.register(EmployeeDetail,EmployeeDetailAdmin)
+
 
 # Register your models here.
+
--- polls/models.py
+++ polls/models.py
@@ -4,9 +4,26 @@ from django.db import models
 
 class Cliente(models.Model):
     name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20, default = '')
     identification = models.IntegerField()
     def __str__(self):
         return self.name
 
+class Department (models.Model):
+    departmen_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.departmen_name
+
+class Employee (models.Model):
+    employee_id = models.IntegerField()
+    name = models.CharField(max_length=15,default='')
+    last_name = models.CharField(max_length=10, default='')
+    department_name = models.ForeignKey("Department", on_delete=models.SET_DEFAULT,default='1')
+    hours = models.IntegerField(default=8)
+    salary = models.IntegerField(default=100)
+    active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
+
 # Create your models here.
+
